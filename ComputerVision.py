10b59e8bcdb52c6414bf3a817c6466157c9b0de6
peterhannibalhildorf <s224274@dtu.student.dk>
2024-06-05 12:23:44 +0200

    [feat] hvid mask er gjort bedre med nye billeder

--- ComputerVision.py
+++ ComputerVision.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-
 def load_image(image_path):
     image = cv2.imread(image_path)
     if image is None:
@@ -9,47 +8,21 @@ def load_image(image_path):
         return None
     return image
 
-"""
-def equalize_histogram(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-
-    v = cv2.equalizeHist(v)
-
-    final_hsv = cv2.merge((h, s, v))
-    image_eq = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-    return image_eq
-
-
-def adjust_brightness(image, value=30):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-
-    lim = 255 - value
-    v[v > lim] = 255
-    v[v <= lim] += value
-
-    final_hsv = cv2.merge((h, s, v))
-    image_bright = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-    return image_bright
-"""
-
-# For at finde balls nemmere
-def find_balls_hsv(image, min_size=50, max_size=1000):  # Størrelsen af hvid, der skal findes
+def find_balls_hsv(image, min_size=300, max_size=1000):  # Størrelsen af hvid, der skal findes
     # Convert the image to HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    white_lower = np.array([0, 0, 180], dtype="uint8")
-    white_upper = np.array([190, 75, 255], dtype="uint8")
+    # Define range for white color in HSV
+    white_lower = np.array([0, 0, 200], dtype="uint8")
+    white_upper = np.array([180, 60, 255], dtype="uint8")
 
+    # Threshold the HSV image to get only white colors
     white_mask = cv2.inRange(hsv_image, white_lower, white_upper)
 
-    """
     # Use morphological operations to clean up the mask
     kernel = np.ones((5, 5), np.uint8)
     white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_CLOSE, kernel)
     white_mask = cv2.morphologyEx(white_mask, cv2.MORPH_OPEN, kernel)
-    """
 
     cv2.imshow('Processed Image', white_mask)
     cv2.waitKey(0)
@@ -57,11 +30,6 @@ def find_balls_hsv(image, min_size=50, max_size=1000):  # Størrelsen af hvid, d
 
     # Find contours
     contours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Filter contours by size
-    # ball_contours = [cnt for cnt in contours if min_size <= cv2.contourArea(cnt) <= max_size]
-
-
 
     ball_contours = []
 
@@ -73,29 +41,17 @@ def find_balls_hsv(image, min_size=50, max_size=1000):  # Størrelsen af hvid, d
             if perimeter == 0:
                 continue
             circularity = 4 * np.pi * (area / (perimeter * perimeter))
-            if 0.4 <= circularity <= 1.2:  # Adjust thresholds as needed for your specific conditions
+            if 0.7 <= circularity <= 1.2:  # Adjust thresholds as needed for your specific conditions
                 ball_contours.append(cnt)
 
     return ball_contours
 
-
 def image_to_cartesian(image_point, origin):  # Funktionen som converter til koordinater
-    """
-    Convert an image point to Cartesian coordinates with the given origin.
-
-    Parameters:
-        image_point (tuple): The point (x, y) in image coordinates.
-        origin (tuple): The origin (x, y) in image coordinates.
-
-    Returns:
-        (tuple): The point in Cartesian coordinates.
-    """
     x, y = image_point
     origin_x, origin_y = origin
     cartesian_x = x - origin_x
     cartesian_y = origin_y - y  # Invert the y-axis
     return cartesian_x, cartesian_y
-
 
 def process_image(image):
     lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
@@ -120,7 +76,6 @@ def process_image(image):
     result = image.copy()
     cv2.drawContours(result, filtered_contours, -1, (0, 255, 0), 2)
 
-    # 3. forsøg på at sætte koordinater på bunden af venstre hjørne
     # Initialize variables to store the extreme points
     min_x = float('inf')
     max_y = -1
@@ -145,8 +100,6 @@ def process_image(image):
                 x = n[i]
                 y = n[i + 1]
 
-                # String containing the co-ordinates.
-                string = str(x) + " " + str(y)
                 # Update the min_x and max_y for the bottom left corner detection
                 if y > max_y or (y == max_y and x < min_x):
                     min_x = x
@@ -156,28 +109,8 @@ def process_image(image):
             i = i + 1
 
     # find balls and process each contour
-    ball_contours = find_balls_hsv(image, min_size=50, max_size=1000)
+    ball_contours = find_balls_hsv(image, min_size=300, max_size=1000)
     print(f"Found {len(ball_contours)} balls initially.")
-
-    """
-    # If not all balls are found, try adjusting the brightness and find again
-    if len(ball_contours) < 12:  # assuming you are looking for 12 balls
-        for i in range(10, 151, 10):  # Adjust range and increment as needed
-            print(f"Adjusting brightness by {i}.")
-            bright_image = adjust_brightness(image, value=i)
-            ball_contours = find_balls_hsv(bright_image, min_size=100, max_size=900)
-            print(f"Found {len(ball_contours)} balls after brightness adjustment.")
-            if len(ball_contours) >= 12:
-                break
-
-
-    # If still not all balls are found, try histogram equalization and find again
-    if len(ball_contours) < 12:  # assuming you are looking for 12 balls
-        print("Applying histogram equalization.")
-        eq_image = equalize_histogram(image)
-        ball_contours = find_balls_hsv(eq_image, min_size=100, max_size=900)
-        print(f"Found {len(ball_contours)} balls after histogram equalization.")  
-    """
 
     for i, contour in enumerate(ball_contours, 1):
         # Compute the bounding rectangle for each ball contour
@@ -194,11 +127,6 @@ def process_image(image):
             # Now you can use `cartesian_coords` as needed
             print(f"Ball {i} Cartesian Coordinates: {cartesian_coords}")
 
-            # Converts the coordinates to string and sends to server
-            # coords_str = f"{cartesian_coords[0]},{cartesian_coords[1]}"
-
-            # return coords_str
-
     # Draw a circle at the detected bottom left corner
     if bottom_left_corner is not None:
         cv2.circle(image, bottom_left_corner, 10, (0, 0, 255), -1)
@@ -208,14 +136,8 @@ def process_image(image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
 if __name__ == "__main__":
-    # images/WIN_20240410_10_46_06_Pro.jpg
-    # images/Bane 3 med Gule/WIN_20240207_09_22_41_Pro.jpg
-    # images/Bane 3 med Gule/WIN_20240207_09_23_43_Pro.jpg
-    # images/bolde_æg_robot_bane_3.jpg
-
-    image_path = "images/WIN_20240410_10_46_06_Pro.jpg"  # Path to your image
+    image_path = "/Users/peterhannibalhildorf/PycharmProjects/GolfBotProject/images/Bane 4 3 ugers/WIN_20240605_10_26_58_Pro.jpg"  # Path to your image
     image = cv2.imread(image_path)
     if image is not None:
         process_image(image)
