b694ec0f253b1fc96574b564b8f80e10593072c4
Anders Jefsen <andersjeff397@gmail.com>
2024-06-13 15:09:31 +0200

    refactor: add method to correct robot position

--- ComputerVision.py
+++ ComputerVision.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import math
 class ImageProcessor:
     corners = {'bottom_left': None, 'bottom_right': None, 'top_left': None, 'top_right': None}
     scale_factors = {'x_scale': None, 'y_scale': None}
@@ -449,6 +449,37 @@ class ImageProcessor:
 
         return cartesian_coords, output_Image
 
+
+    @staticmethod
+    def get_corrected_position(before_robot_coordinates:tuple):
+        x=before_robot_coordinates[0]
+        y=before_robot_coordinates[1]
+        map_width = 166.5
+        map_height = 121.8
+        robot_body_height = 30
+        camera_x = 77  # centered camera position (x-coordinate)
+        camera_y = 51  # centered camera position (y-coordinate)
+        # distance from camera to robot base (account for camera offset)
+        distance_to_base = math.sqrt((x - camera_x)**2 + (y - camera_y)**2)
+
+        # make sure x and y are within map boundaries 
+        if x < 0 or x > map_width:
+            raise ValueError("X coordinate is outside map boundaries")
+        if y < 0 or y > map_height:
+            raise ValueError("Y coordinate is outside map boundaries")
+
+        # calc angle between camera and top of robot
+        angle = math.atan2(robot_body_height, distance_to_base)
+
+        # Correct the x and y coordinates
+        corrected_x = x + distance_to_base * math.cos(angle)
+        corrected_y = y + distance_to_base * math.sin(angle)
+
+        corrected_position_robot = [corrected_x, corrected_y]
+
+        return corrected_position_robot
+
+
     #GKOPERGPOKEROPKG KOM NU JOHAN VS CODE STINKER
     @staticmethod
     def process_robot(indput_Image,output_Image):
@@ -463,8 +494,9 @@ class ImageProcessor:
         if(contours is not None):
             midtpunkt,angle,output_Image = ImageProcessor.calculate_robot_midpoint_and_angle(contours, output_Image)
         
-
+        
         return midtpunkt, angle, output_Image
+    
     
     @staticmethod
     def process_image(image):
