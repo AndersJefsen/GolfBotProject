47cd825e35ecc28b1faf0ce5353274eff00c0e46
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2024-06-15 14:44:55 +0200

    small fix

--- ComputerVision.py
+++ ComputerVision.py
@@ -277,7 +277,7 @@ class ImageProcessor:
     def find_robot(indput_Image, min_size=100, max_size=400):
        
       
-        blue_lower = np.array([80, 50, 100], dtype="uint8")
+        blue_lower = np.array([80, 66, 100], dtype="uint8")
         blue_upper = np.array([131, 255, 255], dtype="uint8")
         blue_mask=ImageProcessor.apply_hsv_filter(indput_Image, blue_lower,blue_upper)
 
